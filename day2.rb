262d9d9c14cae072d4297d1a2e4d8fa007f7e3dc
Simen Eilevstjønn <simene@lyse.net>
2022-12-02 17:14:59 +0100

    With bytes map

--- day2.rb
+++ day2.rb
@@ -2,4 +2,4 @@
 # Loss is + 2 mod 3
 # Win is + 1 mod 3
 # Rock: 1, paper: 2, scissors: 3
-p gets(nil).split(/$/).map{|x|x.split.map(&:ord)}.map{|a,b|3*(b-88)+1+(a+b-1)%3}.sum
+p gets(nil).split("\n").map(&:bytes).map{|a,_,b|3*(b-88)+1+(a+b-1)%3}.sum

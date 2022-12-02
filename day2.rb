90aa4b961d400afe92ddfb5c6a400996c52d549d
Simen Eilevstjønn <simene@lyse.net>
2022-12-02 17:29:10 +0100

    Remove bytes map

--- day2.rb
+++ day2.rb
@@ -2,4 +2,4 @@
 # Loss is + 2 mod 3
 # Win is + 1 mod 3
 # Rock: 1, paper: 2, scissors: 3
-p STDIN.each_line.map(&:bytes).map{|a,_,b|3*b-263+(a+b-1)%3}.sum
+p STDIN.each_line.map{|x|a,_,b=x.bytes;3*b-263+(a+b-1)%3}.sum

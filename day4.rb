5c0e86da5bd82f1d31eb2587d4103d6e4f8280c4
Simen Eilevstjønn <simene@lyse.net>
2022-12-04 10:40:39 +0100

    Working part 2

--- day4.rb
+++ day4.rb
@@ -1,2 +1,1 @@
-l=STDIN.each_line.map{|x|a,b,c,d=x.split(/[\,,-]/).map(&:to_i);(a..b).to_a+(c..d).to_a}
-p l.map{|x|l.map{|y|((x&y).any?)?1:0}.sum}.sum-1000
+p STDIN.each_line.map{|x|a,b,c,d=x.split(/[\,,-]/).map(&:to_i);(((a..b).to_a&(c..d).to_a).any?)?1:0}.sum

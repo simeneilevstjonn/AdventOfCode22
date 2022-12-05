c992618c24f8b7191f0fe768f2801856b508da1d
Simen Eilevstjønn <simene@lyse.net>
2022-12-05 14:10:22 +0100

    Shorten

--- day5.rb
+++ day5.rb
@@ -2,5 +2,5 @@ a,b=`cat`.split("
 
 ")
 q=a.scan(/[A-Z]| {4}/).each_slice(a[-2].to_i).to_a.transpose.map{|y|y.filter{|x|x.size<2}}.map(&:reverse)
-b.scan(/\d+/).map(&:to_i).each_slice(3){|a,b,c|q[c-1]+=q[b-1].pop(a)}
-p q.map{|x|x[-1]}.join
+b.scan(/\d+/).map(&:to_i).each_slice(3){|a,b,c|q[c-1]+=q[b-1].pop a}
+p q.sum("",&:last)

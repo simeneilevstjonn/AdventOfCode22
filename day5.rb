644f9fe05d55875299871f4ba7ff132e43352ca4
Simen Eilevstjønn <simene@lyse.net>
2022-12-05 16:22:51 +0100

    Simplify regex

--- day5.rb
+++ day5.rb
@@ -1,6 +1,6 @@
 a,b=`cat`.split("
 
 ")
-q=a.lines.map{|y|y.scan(/\w|(?= )   (?= )/)}.reverse.transpose.map{|y|y.filter{|x|/ /!~x}}
+q=a.lines.map{|y|y.scan(/\w|    /)}.reverse.transpose.map{|y|y.filter{|x|/ /!~x}}
 b.scan(/\d+/).map(&:to_i).each_slice(3){|a,b,c|q[c-1]+=q[b-1].pop a}
 p q.sum("",&:last)

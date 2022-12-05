6ea8d532a86dba0569cd1c52dde8568c04fd4cef
Simen Eilevstjønn <simene@lyse.net>
2022-12-05 13:34:30 +0100

    Working part 1

--- day5.rb
+++ day5.rb
@@ -3,7 +3,6 @@ a,b=gets(nil).split("
 ");
 c=a.chomp[-2].to_i
 q=[[]]*c
-a.scan(/[A-Z]|\s{4}/).each_slice(c).map.with_index{|x,i|q[i]=[x]+q[i]}
-p q
+a.scan(/[A-Z]|\s{4}/).each_slice(c){|y|y.map.with_index{|x,i|q[i]=[x]+q[i]if x.size<2}}
 b.scan(/\d+/).map(&:to_i).each_slice(3).map{|a,b,c|(1..a).map{q[c-1]+=[q[b-1].pop]}}
-p q.join{|x|x[-1]}
+p q.map{|x|x[-1]}.join

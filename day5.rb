5779fa47b281970aa3276cb639e2d7acba3053e9
Simen Eilevstjønn <simene@lyse.net>
2022-12-05 13:54:26 +0100

    Shorten regex

--- day5.rb
+++ day5.rb
@@ -3,6 +3,6 @@ a,b=`cat`.split("
 ")
 c=a[-2].to_i
 q=[[]]*c
-a.scan(/[A-Z]|\s{4}/).each_slice(c){|y|y.map.with_index{|x,i|q[i]=[x]+q[i]if x.size<2}}
+a.scan(/[A-Z]| {4}/).each_slice(c){|y|y.map.with_index{|x,i|q[i]=[x]+q[i]if x.ord>32}}
 b.scan(/\d+/).map(&:to_i).each_slice(3){|a,b,c|q[c-1]+=q[b-1].pop(a)}
 p q.map{|x|x[-1]}.join

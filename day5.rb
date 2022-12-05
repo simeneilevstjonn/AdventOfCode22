78d1bd04f58013c62469c632d13cbec54a8fc01b
Simen Eilevstjønn <simene@lyse.net>
2022-12-05 13:41:29 +0100

    cat

--- day5.rb
+++ day5.rb
@@ -1,6 +1,7 @@
-a,b=gets(nil).split("
+a,b=`cat`.split("
 
-");c=a[-2].to_i
+")
+c=a[-2].to_i
 q=[[]]*c
 a.scan(/[A-Z]|\s{4}/).each_slice(c){|y|y.map.with_index{|x,i|q[i]=[x]+q[i]if x.size<2}}
 b.scan(/\d+/).map(&:to_i).each_slice(3){|a,b,c|q[c-1]+=q[b-1].pop(a)}

dcceaf376ea408f45f0bbc4719e6a3e0f3eed7be
Simen Eilevstjønn <simene@lyse.net>
2022-12-05 15:17:21 +0100

    Back to 95 l4

--- day5.rb
+++ day5.rb
@@ -1,6 +1,6 @@
 a,b=`cat`.split("
 
 ")
-q=a.lines.map{|y|y.chars.each_slice(4).to_a}.reverse.transpose.map{|y|y.filter_map{|x|x[1]if x[1].ord>32}}
+q=a.lines.map{|y|y.scan(/.{1,4}/).map{|x|x[1]}}.reverse.transpose.map{|y|y.filter{|x|x.ord>32}}
 b.scan(/\d+/).map(&:to_i).each_slice(3){|a,b,c|q[c-1]+=q[b-1].pop a}
 p q.sum("",&:last)

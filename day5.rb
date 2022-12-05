c71c478f21aeeac894aa11f05743f02e27b509c9
Simen Eilevstjønn <simene@lyse.net>
2022-12-05 15:12:05 +0100

    Rewrite working, but longer

--- day5.rb
+++ day5.rb
@@ -1,7 +1,6 @@
 a,b=`cat`.split("
 
 ")
-#q=a.scan(/\w| {4}/).each_slice(a[-2].to_i).to_a.transpose.map{|y|y.filter{|x|x.size<2}.reverse}
-q=a.lines.map{|y|y.chars.each_slice(4).map{|x|x[1]}}.reverse.transpose.map{|y|y.filter{|x|x.ord>32}}
+q=a.lines.map{|y|y.chars.each_slice(4).to_a}.reverse.transpose.map{|y|y.filter_map{|x|x[1]if x[1].ord>32}}
 b.scan(/\d+/).map(&:to_i).each_slice(3){|a,b,c|q[c-1]+=q[b-1].pop a}
 p q.sum("",&:last)

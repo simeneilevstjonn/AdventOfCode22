6d81a01886897994b6a112a44f97bb485c05e4bf
Simen Eilevstjønn <simene@lyse.net>
2022-12-05 14:05:30 +0100

    Replace with transpose

--- day5.rb
+++ day5.rb
@@ -1,8 +1,6 @@
 a,b=`cat`.split("
 
 ")
-c=a[-2].to_i
-q=[[]]*c
-a.scan(/[A-Z]| {4}/).each_slice(c){|y|y.map.with_index{|x,i|q[i]=[x]+q[i]if x.ord>32}}
+q=a.scan(/[A-Z]| {4}/).each_slice(a[-2].to_i).to_a.transpose.map{|y|y.filter{|x|x.size<2}}.map(&:reverse)
 b.scan(/\d+/).map(&:to_i).each_slice(3){|a,b,c|q[c-1]+=q[b-1].pop(a)}
 p q.map{|x|x[-1]}.join

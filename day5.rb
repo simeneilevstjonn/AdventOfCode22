4245a33bb75e60bac243fca7c74b515fdee37d58
Simen Eilevstjønn <simene@lyse.net>
2022-12-05 14:16:58 +0100

    Change regex

--- day5.rb
+++ day5.rb
@@ -1,6 +1,6 @@
 a,b=`cat`.split("
 
 ")
-q=a.scan(/[A-Z]| {4}/).each_slice(a[-2].to_i).to_a.transpose.map{|y|y.filter{|x|x.size<2}.reverse}
+q=a.scan(/\w| {4}/).each_slice(a[-2].to_i).to_a.transpose.map{|y|y.filter{|x|x.size<2}.reverse}
 b.scan(/\d+/).map(&:to_i).each_slice(3){|a,b,c|q[c-1]+=q[b-1].pop a}
 p q.sum("",&:last)

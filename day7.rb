20b2c1edd1fe956088109f926fd0adf4de246951
Simen Eilevstjønn <simene@lyse.net>
2022-12-07 16:36:19 +0100

    Reduce slice to one char

--- day7.rb
+++ day7.rb
@@ -1,5 +1,5 @@
 t,p,d={},[],[]
-$<.map{|x|x[0,3]=="$ c"?(a=x[5..-2];a<"."?p=[]:(a<"0"?p.pop : p+=[a])):((a=t;p.map{|y|a=a[y]};b,c=x.split;a[c]=b>"a"?{}: b.to_i)if x>"$")}
+$<.map{|x|x[2]=="c"?(a=x[5..-2];a<"."?p=[]:(a<"0"?p.pop : p+=[a])):((a=t;p.map{|y|a=a[y]};b,c=x.split;a[c]=b>"a"?{}: b.to_i)if x>"$")}
 f=->(m){t=0;m.map{|k,a|(a.is_a? Hash)?t+=f.(a): t+=a};d+=[t];t}
 s=f.(t)-4e7
 p d.filter{|x|x>=s}.min

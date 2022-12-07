87b0ea0dde941928ce4e6ae97d3fee874caf6f04
Simen Eilevstjønn <simene@lyse.net>
2022-12-07 17:56:34 +0100

    Split in beginning of map

--- day7.rb
+++ day7.rb
@@ -1,5 +1,5 @@
 t,p,d={},[],[]
-$<.map{|x|x[2]==?c?(a=x[5..-2];a<?.?p=[]:(a<?0?p.pop : p+=[a])):((a=t;p.map{|y|a=a[y]};b,c=x.split;a[c]=b>?a?{}: b.to_i)if"$"<x)}
+$<.map{|x|u,v,w=x.split;w!=nil ?(w<?.?p=[]:(w<?0?p.pop : p+=[w])):((a=t;p.map{|y|a=a[y]};a[v]=u>?a?{}: u.to_i)if"$"<x)}
 f=->(m){t=0;m.map{|k,a|t+=(a.is_a? Hash)?f.(a):a};d+=[t];t}
 s=f.(t)-4e7
 p d.filter{|x|x>=s}.min

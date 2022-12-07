1b11971d6b8acd75d02cded57be493a47b7c8a25
Simen Eilevstjønn <simene@lyse.net>
2022-12-07 15:51:18 +0100

    Use inequalities for checking strings

--- day7.rb
+++ day7.rb
@@ -1,5 +1,5 @@
 t,p,d={},[],[]
-$<.map{|x|x[0,4]=="$ cd"?(a=x[5...-1];a=="/"?p=[]:(a==".."?p.pop : p+=[a])):((a=t;p.map{|y|a=a[y]};b,c=x.split;a[c]=b=="dir"?{}: b.to_i)if x!="$ ls\n")}
+$<.map{|x|x[0,3]=="$ c"?(a=x[5...-1];a<"."?p=[]:(a<"0"?p.pop : p+=[a])):((a=t;p.map{|y|a=a[y]};b,c=x.split;a[c]=b>"a"?{}: b.to_i)if x>"$")}
 f=->(m){t=0;m.each{|k,a|(a.is_a? Hash)?t+=f.call(a): t+=a};d+=[t];t}
 s=f.call(t)-4e7
 p d.filter{|x|x>=s}.min

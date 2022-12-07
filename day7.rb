fc46931224206e3f9a149bc232ef4a73c6670c96
Simen Eilevstjønn <simene@lyse.net>
2022-12-07 13:35:48 +0100

    Simplify long for down to map

--- day7.rb
+++ day7.rb
@@ -1,21 +1,5 @@
 t={};p=[]
-for x in $<.each_line do
-    if x.include?"$ cd"
-        a = x.delete_prefix("$ cd ").chomp
-        if a=="/"
-            p = []
-        elsif a==".."
-            p.pop
-        else
-            p+=[a]
-        end
-    elsif x!="$ ls\n"
-        a = t
-        p.map{|y|a=a[y]}
-        b,c=x.split
-        a[c]=b=="dir"?{}: b.to_i
-    end
-end
+$<.map{|x|x[0,4]=="$ cd"?(a=x[5...-1];a=="/"?p=[]:(a==".."?p.pop : p+=[a])):((a=t;p.map{|y|a=a[y]};b,c=x.split;a[c]=b=="dir"?{}: b.to_i)if x!="$ ls\n")}
 def dfs(m,d)
     t = 0
     m.each{|k,a|t+=a if a.is_a? Integer;t+=dfs(a,d)unless a.is_a? Integer}
@@ -23,5 +7,5 @@ def dfs(m,d)
     return t
 end
 d=[]
-s=30000000-70000000+dfs(t,d)
+s=dfs(t,d)-4e7
 p d.filter{|x|x>=s}.min

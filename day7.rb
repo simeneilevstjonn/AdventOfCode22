1645b69bffb3ec7de0a99afa0e26f34979dd4e5d
Simen Eilevstjønn <simene@lyse.net>
2022-12-07 13:51:47 +0100

    replace with global

--- day7.rb
+++ day7.rb
@@ -1,5 +1,5 @@
-t,p,d={},[],[]
+t,p,@d={},[],[]
 $<.map{|x|x[0,4]=="$ cd"?(a=x[5...-1];a=="/"?p=[]:(a==".."?p.pop : p+=[a])):((a=t;p.map{|y|a=a[y]};b,c=x.split;a[c]=b=="dir"?{}: b.to_i)if x!="$ ls\n")}
-f=->(m,d){t=0;m.each{|k,a|t+=a if a.is_a? Integer;t+=f.call(a,d)if !a.is_a? Integer};d.append t;t}
-s=f.call(t,d)-4e7
-p d.filter{|x|x>=s}.min
+f=->(m){t=0;m.each{|k,a|t+=a if a.is_a? Integer;t+=f.call(a)if !a.is_a? Integer};@d.append t;t}
+s=f.call(t)-4e7
+p @d.filter{|x|x>=s}.min

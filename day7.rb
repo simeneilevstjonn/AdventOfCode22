5acbed7c4bfff61d2d337fa1aa00254d4c35b82e
Simen Eilevstjønn <simene@lyse.net>
2022-12-07 13:39:16 +0100

    Simplify method to lambda

--- day7.rb
+++ day7.rb
@@ -1,11 +1,5 @@
-t={};p=[]
+t,p,d={},[],[]
 $<.map{|x|x[0,4]=="$ cd"?(a=x[5...-1];a=="/"?p=[]:(a==".."?p.pop : p+=[a])):((a=t;p.map{|y|a=a[y]};b,c=x.split;a[c]=b=="dir"?{}: b.to_i)if x!="$ ls\n")}
-def dfs(m,d)
-    t = 0
-    m.each{|k,a|t+=a if a.is_a? Integer;t+=dfs(a,d)unless a.is_a? Integer}
-    d.append(t)
-    return t
-end
-d=[]
-s=dfs(t,d)-4e7
+f= ->(m,d){t=0;m.each{|k,a|t+=a if a.is_a? Integer;t+=f.call(a,d)unless a.is_a? Integer};d.append t;t}
+s=f.call(t,d)-4e7
 p d.filter{|x|x>=s}.min

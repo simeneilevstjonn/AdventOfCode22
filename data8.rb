cbcde121c60a82d4873f9930ed0e0a737ece0c01
Simen Eilevstjønn <simene@lyse.net>
2022-12-08 09:04:03 +0100

    More attempts, but it does not work

--- data8.rb
+++ data8.rb
@@ -4,27 +4,48 @@ for i in 0...g.size
     for j in 0...g[0].size
         s=[0,0,0,0]
         x = g[i][0...j].reverse
+        if x.size == 1
+            s[0] = 1
+        end
         for k in 1...x.size
             if x[k] > x[k-1]
                 s[0] += 1
+            
+            else
+                break
             end
         end    
         x=g[i][j+1..-1]
+        if x.size == 1
+            s[1] = 1
+        end
         for k in 1...x.size
             if x[k] > x[k-1]
                 s[1] += 1
+            else
+                break
             end
         end   
         x = g[0...i].map{|x|x[j]}.reverse
+        if x.size == 1
+            s[2] = 1
+        end
         for k in 1...x.size
             if x[k] > x[k-1]
                 s[2] += 1
+            else
+                break
             end
         end 
         x=g[i+1..-1].map{|x|x[j]}
+        if x.size == 1
+            s[3] = 1
+        end
         for k in 1...x.size
             if x[k] > x[k-1]
                 s[3] += 1
+            else
+                break
             end
         end 
         c+=[s.inject(:*)]

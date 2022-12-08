f3cf38596e8f5d49e8053c86c6e201c85ecb6ae8
Simen Eilevstjønn <simene@lyse.net>
2022-12-08 18:15:09 +0100

    Save one more char by saving size

--- day8.rb
+++ day8.rb
@@ -1,2 +1,2 @@
 g=`cat`.split
-p (0...g.size).map{|i|(0...g[0].size).map{|j|s=[g[i][0...j].reverse,g[i][j+1..-1],g[0...i].map{|x|x[j]}.reverse,g[i+1..-1].map{|x|x[j]}];s.filter(&:empty?).any? ? 0:s.map{|x|k=0;(0...x.size).map{|c|k=c;break if x[k]>=g[i][j]};k==x.size ? k :1+k}.inject(:*)}.max}.max
+p (0...g.size).map{|i|(0...g[0].size).map{|j|s=[g[i][0...j].reverse,g[i][j+1..-1],g[0...i].map{|x|x[j]}.reverse,g[i+1..-1].map{|x|x[j]}];s.filter(&:empty?).any? ? 0:s.map{|x|k=0;l=x.size;(0...l).map{|c|k=c;break if x[k]>=g[i][j]};k==l ? k :1+k}.inject(:*)}.max}.max

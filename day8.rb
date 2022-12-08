e6f1521d6685e34e137ef72f5ffa571b2e52571e
Simen Eilevstjønn <simene@lyse.net>
2022-12-08 17:32:04 +0100

    Something that works in ruby

--- day8.rb
+++ day8.rb
@@ -1,54 +1,18 @@
-g=$<.map(&:bytes)
+g=$<.map{|x|x.chomp.bytes}
 c=[]
 for i in 0...g.size
     for j in 0...g[0].size
-        s=[0,0,0,0]
-        x = g[i][0...j].reverse
-        if x.size == 1
-            s[0] = 1
-        end
-        for k in 1...x.size
-            if x[k] > x[k-1]
-                s[0] += 1
-            
-            else
-                break
+        s = [g[i][0...j].reverse, g[i][j+1..-1], g[0...i].map{|x|x[j]}.reverse, g[i+1..-1].map{|x|x[j]}]
+        a=1
+        next if s.filter(&:empty?).any?
+        for x in s
+            k = 0
+            while k < x.size and x[k] < g[i][j]
+                k+=1
             end
-        end    
-        x=g[i][j+1..-1]
-        if x.size == 1
-            s[1] = 1
+            a*=k + (k==x.size ? 0:1)
         end
-        for k in 1...x.size
-            if x[k] > x[k-1]
-                s[1] += 1
-            else
-                break
-            end
-        end   
-        x = g[0...i].map{|x|x[j]}.reverse
-        if x.size == 1
-            s[2] = 1
-        end
-        for k in 1...x.size
-            if x[k] > x[k-1]
-                s[2] += 1
-            else
-                break
-            end
-        end 
-        x=g[i+1..-1].map{|x|x[j]}
-        if x.size == 1
-            s[3] = 1
-        end
-        for k in 1...x.size
-            if x[k] > x[k-1]
-                s[3] += 1
-            else
-                break
-            end
-        end 
-        c+=[s.inject(:*)]
+        c+=[a]
     end
 end
 p c.max

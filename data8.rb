1afc0419b045e4e872e36d90d78bc3b3420733c8
Simen Eilevstjønn <simene@lyse.net>
2022-12-08 08:46:14 +0100

    Attempt day 8

---
+++ data8.rb
@@ -0,0 +1,21 @@
+g=$<.map(&:bytes)
+c=0
+for i in 0...g.size
+    for j in 0...g[0].size
+        h = g[i][j]
+        # Slice on top
+        if (g[i][0...j].max||10 < h)
+            c+=1
+        # Slice below
+        elsif (g[i][j+1..-1].max||10 < h)
+            c+=1
+        # Slice on left
+        elsif (g[0...i].map{|x|x[j]}.max||10 < h)
+            c+=1
+        # Slice of right
+        elsif (g[i+1..-1].map{|x|x[j]}.max||10 < h)
+            c+=1
+        end
+    end
+end
+p c

9f323b2beb3a4fa07219d23a57ee1e5991e32145
Simen Eilevstjønn <simene@lyse.net>
2022-12-09 12:28:31 +0100

    Day 9 working verbose python part 1

---
+++ day9.py
@@ -0,0 +1,42 @@
+data = [[i.split()[0], int(i.split()[1])] for i in open("data9.txt","r").read().split("\n")]
+head = [0, 0]
+tail = [0, 0]
+tailvisits = []
+
+for direction, distance in data:
+    for _ in range(distance):
+        if direction == "U":
+            head[0] += 1
+        elif direction == "D":
+            head[0] -= 1
+        elif direction == "L":
+            head[1] -= 1
+        elif direction == "R":
+            head[1] += 1
+        
+        # Move tail if not adjacent. Eucledean distance above sqrt(2)
+        if (tail[0] - head[0])**2 + (tail[1] - head[1])**2 > 2:
+            # Linear
+            if tail[0] == head[0]:
+                tail[1] += (head[1] > tail[1]) - (head[1] < tail[1])
+            elif tail[1] == head[1]:
+                tail[0] += (head[0] > tail[0]) - (head[0] < tail[0])
+            # Diagonal
+            else:
+                if (head[0] > tail[0]) and (head[1] > tail[1]):
+                    tail[0] += 1
+                    tail[1] += 1
+                elif (head[0] > tail[0]) and (head[1] < tail[1]):
+                    tail[0] += 1
+                    tail[1] -= 1
+                elif (head[0] < tail[0]) and (head[1] < tail[1]):
+                    tail[0] -= 1
+                    tail[1] -= 1
+                elif (head[0] < tail[0]) and (head[1] > tail[1]):
+                    tail[0] -= 1
+                    tail[1] += 1
+
+        # Add tails position
+        tailvisits.append((tail[0], tail[1]))
+
+print(len(set(tailvisits)))

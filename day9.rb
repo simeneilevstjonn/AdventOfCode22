04712a222a4d308ad8fe19bf2ea09c86c0979de4
Simen Eilevstjønn <simene@lyse.net>
2022-12-09 15:34:41 +0100

    Get result directly from map with flatten

--- day9.rb
+++ day9.rb
@@ -1,4 +1,2 @@
 r=(0..9).map{[0,0]}
-v=[]
-$<.map{|x|d,b=x.split;(1..b.to_i).map{r[0][0]+=d>?T?1:d<?E?-1:0;r[0][1]+=d==?R?1:d==?L?-1:0;(1..9).map{|i|e=r[i];f,g=r[i-1];h,l=e;(e[0]+=f>h ?1:((f<h)?-1:0);e[1]+=g>l ?1:((g<l)?-1:0))if(h-f)**2+(l-g)**2>2;v.append r[9][0,2]}}}
-p v.uniq.size
+p $<.map{|x|d,b=x.split;(1..b.to_i).map{r[0][0]+=d>?T?1:d<?E?-1:0;r[0][1]+=d==?R?1:d==?L?-1:0;(1..9).map{|i|e=r[i];f,g=r[i-1];h,l=e;(e[0]+=f>h ?1:((f<h)?-1:0);e[1]+=g>l ?1:((g<l)?-1:0))if(h-f)**2+(l-g)**2>2;r[9][0,2]}}}.flatten(2).uniq.size

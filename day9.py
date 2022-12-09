e4451f330d1d73a7916b7d10c21ddadaa350d6ef
Simen Eilevstjønn <simene@lyse.net>
2022-12-09 12:54:10 +0100

    Generalisation attempt

--- day9.py
+++ day9.py
@@ -1,26 +1,25 @@
-data = [[i.split()[0], int(i.split()[1])] for i in open("data9.txt","r").read().split("\n")]
-head = [0, 0]
-tail = [0, 0]
+data = [[i.split()[0], int(i.split()[1])] for i in open("testdata9.txt","r").read().split("\n")]
+rope = [[0, 0]] * 10
 tailvisits = []
 
 for direction, distance in data:
     for _ in range(distance):
         if direction == "U":
-            head[0] += 1
+            rope[0][0] += 1
         elif direction == "D":
-            head[0] -= 1
+            rope[0][0] -= 1
         elif direction == "L":
-            head[1] -= 1
+            rope[0][1] -= 1
         elif direction == "R":
-            head[1] += 1
+            rope[0][1] += 1
         
         # Move tail if not adjacent. Eucledean distance above sqrt(2)
-        if (tail[0] - head[0])**2 + (tail[1] - head[1])**2 > 2:
-            tail[0] += (head[0] > tail[0]) - (head[0] < tail[0])
-            tail[1] += (head[1] > tail[1]) - (head[1] < tail[1])
-            
+        for i in range(1,10):
+            if (rope[i][0] - rope[i - 1][0])**2 + (rope[i][1] - rope[i - 1][1])**2 > 2:
+                rope[i][0] += (rope[i - 1][0] > rope[i][0]) - (rope[i - 1][0] < rope[i][0])
+                rope[i][1] += (rope[i - 1][1] > rope[i][1]) - (rope[i - 1][1] < rope[i][1])
 
-        # Add tails position
-        tailvisits.append((tail[0], tail[1]))
+            # Add tails position
+            tailvisits.append((rope[9][0], rope[9][1]))
 
 print(len(set(tailvisits)))

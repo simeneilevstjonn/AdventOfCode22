bed5b1b4e65a934c765663ae4e5ae5285b88757a
Simen Eilevstjønn <simene@lyse.net>
2022-12-10 14:04:12 +0100

    Fix newlining

--- day10.rb
+++ day10.rb
@@ -1,2 +1,3 @@
 x=1;c=0
-$<.map{|y|a,b=y.split;print (c%40-x)**2<2 ? ?# : ?.;c+=1;(print (c%40-x)**2<2 ? ?# : ?.;c+=1;x+=b.to_i) if a<?b}
+r=->{print (c%40-x)**2<2??#:?.;c+=1;print "\n"if c%40==0}
+$<.map{|y|a,b=y.split;r.();(r.();x+=b.to_i) if a<?b}

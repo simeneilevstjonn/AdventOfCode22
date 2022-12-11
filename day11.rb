f9a3fc10fd2ba5a064a1dc5c147f595519be2ddf
Simen Eilevstjønn <simene@lyse.net>
2022-12-11 19:02:19 +0100

    Fix type error

--- day11.rb
+++ day11.rb
@@ -1,5 +1,5 @@
 q=[];f=[];t=[];m=[];o=[]
-gets(nil).split("\n\n").map{|x|_,b,c,d,e,a=x.split("\n");q+=[b.scan(/\d+/).map(&:to_i)];o+=[c[19..]];m+=[d[21..].to_i];t+=[e[29..].to_i];f+=[a[30..]]}
+gets(nil).split("\n\n").map{|x|_,b,c,d,e,a=x.split("\n");q+=[b.scan(/\d+/).map(&:to_i)];o+=[c[19..]];m+=[d[21..].to_i];t+=[e[29..].to_i];f+=[a[30..].to_i]}
 c=f.size
 p=[0]*c
 (0..1e5).map{(0...c).map{|i|q[i].map{|old|x=eval(o[i])%m.inject(:*);x%m[i]<1?(q[t[i]]+=[x]):(q[f[i]]+=[x]);p[i]+=1}}}

db569ceec15dcb37192b471b4308bf00143452c5
Simen Eilevstjønn <simene@lyse.net>
2022-12-12 17:55:07 +0100

    Fix ruby implement

--- day12.rb
+++ day12.rb
@@ -4,12 +4,12 @@ w=g[0].size
 e=nil
 s=[]
 (0...h).map{|y|(0...w).map{|x|(s.append [y,x];g[y][x]=97)if (g[y][x]==83||g[y][x]==97);(e=[y,x];g[y][x]=122)if g[y][x]==69}}
-p s.map{|y,x|
-    d=[[1e9]*w]*h
-    d[y][x]=0
-    v=[[0]*w]*h
+p s.map{|o,p|
+    d=(1..h).map{[1e9]*w}
+    d[o][p]=0
+    v=(1..h).map{[0]*w}
     q=Queue.new
-    q.push([y,x])
-    (y,x=q.pop;(v[y][x]=1;((y!=0?[[y-1,x]]:[])+(y!=h-1?[[y+1,x]]:[])+(x!=0?[[y,x-1]]:[])+(x!=w-1?[[y,x+1]]:[])).map{|a,b|(d[a][b]=1+d[y][x];q.push([a,b]))if g[a][b]-1<=g[y][y]})if v[y][x]<1)while !q.empty?
+    q.push [o,p]
+    (y,x=q.pop;(v[y][x]=1;((y!=0?[[y-1,x]]:[])+(y!=h-1?[[y+1,x]]:[])+(x!=0?[[y,x-1]]:[])+(x!=w-1?[[y,x+1]]:[])).map{|a,b|(d[a][b]=1+d[y][x];q.push([a,b]))if g[a][b]-1<=g[y][x]})if v[y][x]<1)while !q.empty?
     d[e[0]][e[1]]
 }.min

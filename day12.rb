eba926f981c8e6307bdeed942b722758e57051ff
Simen Eilevstjønn <simene@lyse.net>
2022-12-12 15:57:49 +0100

    Fix incorrect order of coords

--- day12.rb
+++ day12.rb
@@ -9,7 +9,6 @@ p s.map{|x,y|
     v=[[0]*w]*h
     q=Queue.new
     q.push(s)
-    (x,y=q.pop;(((y!=0?[[y-1,x]]:[])+(y!=h-1?[[y+1,x]]:[])+(x!=0?[[y,x-1]]:[])+(x!=w-1?[[y,x+1]]:[])).map{|a,b|
-        (d[a][b]=1+d[y][x];q.push([a,b]))if g[a][b]-1<=g[y][y]})if !v[y][x])while !q.empty?
+    (y,x=q.pop;(((y!=0?[[y-1,x]]:[])+(y!=h-1?[[y+1,x]]:[])+(x!=0?[[y,x-1]]:[])+(x!=w-1?[[y,x+1]]:[])).map{|a,b|(d[a][b]=1+d[y][x];q.push([a,b]))if g[a][b]-1<=g[y][y]})if !v[y][x])while !q.empty?
     d[e[0]][e[1]]
 }.min

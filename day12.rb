59821c1f4972d4319ebc1435e9b9b3fd40e119c2
Simen Eilevstjønn <simene@lyse.net>
2022-12-12 17:29:25 +0100

    Move visibility assignment and fix if condition

--- day12.rb
+++ day12.rb
@@ -9,6 +9,6 @@ p s.map{|y,x|
     v=[[0]*w]*h
     q=Queue.new
     q.push([y,x])
-    (y,x=q.pop;(((v[y][x]=1;y!=0?[[y-1,x]]:[])+(y!=h-1?[[y+1,x]]:[])+(x!=0?[[y,x-1]]:[])+(x!=w-1?[[y,x+1]]:[])).map{|a,b|(d[a][b]=1+d[y][x];q.push([a,b]))if g[a][b]-1<=g[y][y]})if !v[y][x])while !q.empty?
+    (y,x=q.pop;(v[y][x]=1;((y!=0?[[y-1,x]]:[])+(y!=h-1?[[y+1,x]]:[])+(x!=0?[[y,x-1]]:[])+(x!=w-1?[[y,x+1]]:[])).map{|a,b|(d[a][b]=1+d[y][x];q.push([a,b]))if g[a][b]-1<=g[y][y]})if v[y][x]<1)while !q.empty?
     d[e[0]][e[1]]
 }.min

157e210445918285bcd6f72dfb9e11193d4c54a6
Simen Eilevstjønn <simene@lyse.net>
2022-12-13 15:43:27 +0100

    Day 13 part 2

--- day13.py
+++ day13.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 data = [[eval(i.split("\n")[0]), eval(i.split("\n")[1])] for i in open("data13.txt","r").read().split("\n\n")]
 
 def lteq(a, b):
@@ -25,19 +27,43 @@ def lteq(a, b):
 
         #print("Listcomp", q)
 
-        if q is not None: return q
+        if q != 2: return q
     
     #print("All equal until now", len(b) == len(a), len(b) > len(a))
 
-    return None if len(b) == len(a) else len(b) > len(a)
+    return 2 if len(b) == len(a) else len(b) > len(a)
 
-i = 1
-s = 0
+c,d = [[2]],[[6]]
+
+packets = [c,d]
+
 for a, b in data:
-    #print("Start compare of pair",i)
-    if lteq(a, b):
-        #print(i)
-        s += i
-    i += 1
+    packets.append(a)
+    packets.append(b)
 
-print(s)
+
+
+def partition(A, lo, hi):
+    pivot = A[hi]
+
+    i = lo - 1
+
+    for j in range(lo, hi):
+        if lteq(A[j], pivot):
+            i = i + 1
+            A[j], A[i] = A[i], A[j]
+    
+    i += 1
+    A[hi], A[i] = A[i], A[hi]
+    return i
+
+def quicksort(A, lo, hi):
+    if lo >= hi or lo < 0: 
+        return
+    p = partition(A, lo, hi)
+    quicksort(A, lo, p - 1)
+    quicksort(A, p + 1, hi)
+
+quicksort(packets, 0, len(packets) - 1)
+    
+print((packets.index(c)+1)*(packets.index(d)+1))

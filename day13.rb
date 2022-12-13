ffccc70e74cbe36dcf96dcd878bcd8a759219ff8
Simen Eilevstjønn <simene@lyse.net>
2022-12-13 16:14:16 +0100

    Move class checking

--- day13.rb
+++ day13.rb
@@ -2,10 +2,11 @@ e,f=[[2]],[[6]]
 l=$<.map{|x|eval x}.filter{|x|x}+[e,f]
 c=->(a,b){
     (0...[a.size,b.size].min).map{|i|
-        (a[i]<b[i]? (return -1) : (a[i]>b[i]? (return 1) : next))if a[i].class==Integer&&b[i].class==Integer;
-        a[i]=[a[i]]if a[i].class==Integer;
-        b[i]=[b[i]]if b[i].class==Integer;
-        q=c.(a[i],b[i])
+        g,h=[a[i],b[i]].map{|x|x.class==Integer}
+        (return -1 if a[i]<b[i];return 1 if a[i]>b[i];next)if g&&h;
+        a[i]=[a[i]]if g;
+        b[i]=[b[i]]if h;
+        q=c.(a[i],b[i]);
         return q if q!=0
     };
     return b.size==a.size ? 0 : (b.size > a.size ? -1 : 1)

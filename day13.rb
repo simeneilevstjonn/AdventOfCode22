6ddb50c3b61adb39daa65521ea07d1060005c22a
Simen Eilevstjønn <simene@lyse.net>
2022-12-13 16:34:15 +0100

    e and f do not need two layers of wrapping

--- day13.rb
+++ day13.rb
@@ -1,4 +1,4 @@
-e,f=[[2]],[[6]]
+e,f=[2],[6]
 c=->(a,b){(0...[a.size,b.size].min).map{|i|m,n=a[i],b[i];g,h=[m,n].map{|x|x.class==Integer};(return -1 if m<n;return 1 if m>n;next)if g&&h;a[i]=[m]if g;b[i]=[n]if h;q=c.(a[i],b[i]);return q if q!=0};return b.size==a.size ? 0: b.size>a.size ? -1:1}
 l=($<.map{|x|eval x}.compact+[e,f]).sort{|a,b|c.(a,b)}
 p (1+l.index(e))*(1+l.index(f))

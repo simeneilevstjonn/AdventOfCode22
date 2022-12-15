0db095d79399367709db287ba7c909fa79d2aadb
Simen Eilevstjønn <simene@lyse.net>
2022-12-15 09:35:12 +0100

    Some change to program, still does not work

--- day15.py
+++ day15.py
@@ -2,15 +2,12 @@ import re
 
 data = [list(map(int, re.findall("-*\d+", i))) for i in open("data15.txt", "r").read().split("\n")]
 
-inside = lambda sx, sy, px, py, cx, cy : abs(px - sx) + abs(py - sy) == abs(cx - sx) + abs(cy - sy)
+inside = lambda sx, sy, px, py, cx, cy : abs(px - sx) + abs(py - sy) >= abs(cx - sx) + abs(cy - sy)
 
 count = 0
-for i in range(-10**10, 10**10):
-    if i % 10**8 == 0:
-        print(i)
-    for sx, sy, px, py in data:
-        if px == i:
-            break
+for sx, sy, px, py in data:
+    for i in range(sx - abs(sx - px) - abs(sy - py) - 10**5, sx + abs(sx - px) + abs(sy - py) + 10**5):
         count += inside(sx, sy, px, py, i, 2000000)
+
 
 print(count)

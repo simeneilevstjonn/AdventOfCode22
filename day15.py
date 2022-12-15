d1ece3a8b87f3093e6cf51ccb7daab9ca37fe25e
Simen Eilevstjønn <simene@lyse.net>
2022-12-15 09:37:43 +0100

    Try to remove duplicates

--- day15.py
+++ day15.py
@@ -4,10 +4,10 @@ data = [list(map(int, re.findall("-*\d+", i))) for i in open("data15.txt", "r").
 
 inside = lambda sx, sy, px, py, cx, cy : abs(px - sx) + abs(py - sy) >= abs(cx - sx) + abs(cy - sy)
 
-count = 0
+ins = []
 for sx, sy, px, py in data:
-    for i in range(sx - abs(sx - px) - abs(sy - py) - 10**5, sx + abs(sx - px) + abs(sy - py) + 10**5):
-        count += inside(sx, sy, px, py, i, 2000000)
+    for i in range(sx - abs(sx - px) - abs(sy - py) - 10, sx + abs(sx - px) + abs(sy - py) + 10):
+        if inside(sx, sy, px, py, i, 2000000):ins.append(i)
 
 
-print(count)
+print(len(set(ins)))

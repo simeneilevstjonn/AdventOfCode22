6be7046ac5f8d662d1b2234d44871b3d8a2acfce
Simen Eilevstjønn <simene@lyse.net>
2022-12-18 22:22:40 +0100

    Adjust size to allow for padding around entire grid

--- day18.py
+++ day18.py
@@ -1,10 +1,6 @@
-import sys
-
-sys.setrecursionlimit(10**6)
-
 points = [list(map(int, i.split(","))) for i in open("data18.txt", "r").read().split("\n")]
 
-size = max([max(i) for i in points]) + 1
+size = max([max(i) for i in points]) + 2
 
 space = [[[False] * size for i in range(size)] for j in range(size)]
 visited = [[[False] * size for i in range(size)] for j in range(size)]

116dce56bac962a3d5348b040af1aab1d6879100
Simen Eilevstjønn <simene@lyse.net>
2022-12-18 22:12:24 +0100

    Attempted day 18 part 2 No output is given

--- day18.py
+++ day18.py
@@ -1,12 +1,42 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+
 points = [list(map(int, i.split(","))) for i in open("data18.txt", "r").read().split("\n")]
-area = 0
+
+size = max([max(i) for i in points]) + 1
+
+space = [[[False] * size for i in range(size)] for j in range(size)]
+visited = [[[False] * size for i in range(size)] for j in range(size)]
+
 
 for x, y, z in points:
-    area += [x, y, z + 1] not in points
-    area += [x, y, z - 1] not in points
-    area += [x, y + 1, z] not in points
-    area += [x, y - 1, z] not in points
-    area += [x + 1, y, z] not in points
-    area += [x - 1, y, z] not in points
+    space[z][y][x] = True
+
+area = 0
+
+def dfs(z, y, x):
+    if visited[z][y][x]:
+        return
+    
+    if space[z][y][x]:
+        global area
+        area += 1
+        return
+    
+    if x != 0:
+        dfs(z, y, x - 1)
+    if y != 0:
+        dfs(z, y - 1, x)
+    if z != 0:
+        dfs(z - 1, y, x)
+    if x != size - 1:
+        dfs(z, y, x + 1)
+    if y != size - 1:
+        dfs(z, y + 1, x)
+    if z != size - 1:
+        dfs(z + 1, y, x)
+
+dfs(0,0,0)
 
 print(area)

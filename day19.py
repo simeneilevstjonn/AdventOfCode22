eeceace1a47f82692be5e77e123d34eee8aedcbe
Simen Eilevstjønn <simene@lyse.net>
2022-12-19 16:08:22 +0100

    Try day 19 brute force

---
+++ day19.py
@@ -0,0 +1,70 @@
+import re
+
+data = [list(map(int, re.findall("\d+", i))) for i in open("data19.txt", "r").read().split("\n")]
+
+# Format: id, ore (ore), clay (ore), obs (ore), obs (clay), geode (ore), geode (obs)
+
+def bruteForce(blueprint, robots, resources, time):
+    # Robots and resources are 4 long arrays. [ore, clay, obs, geode]
+
+    if time == 0:
+        return resources[3]
+    
+    # Collect resouces
+    for i in range(4):
+        resources[i] += robots[i]
+
+    # No change
+    geodes = bruteForce(blueprint, robots[::], resources[::], time - 1)
+
+    # Make ore
+    if resources[0] - robots[0] >= blueprint[1]:
+        r = robots[::]
+        r[0] += 1
+
+        s = resources[::]
+        s[0] -= blueprint[1]
+
+        geodes = max(geodes, bruteForce(blueprint, r, s, time - 1))
+    
+    # Make clay
+    if resources[0] - robots[0] >= blueprint[2]:
+        r = robots[::]
+        r[1] += 1
+
+        s = resources[::]
+        s[1] -= blueprint[2]
+
+        geodes = max(geodes, bruteForce(blueprint, r, s, time - 1))
+    
+    # Make obs
+    if resources[0] - robots[0] >= blueprint[3] and resources[2] - robots[2] >= blueprint[4]:
+        r = robots[::]
+        r[2] += 1
+
+        s = resources[::]
+        s[0] -= blueprint[3]
+        s[1] -= blueprint[4]
+
+        geodes = max(geodes, bruteForce(blueprint, r, s, time - 1))
+
+    # Make geode
+    if resources[0] - robots[0] >= blueprint[5] and resources[3] - robots[3] >= blueprint[6]:
+        r = robots[::]
+        r[3] += 1
+
+        s = resources[::]
+        s[0] -= blueprint[5]
+        s[3] -= blueprint[6]
+
+        geodes = max(geodes, bruteForce(blueprint, r, s, time - 1))
+
+    return geodes
+
+# Iterate each blueprint
+sum = 0
+
+for i, x in enumerate(data):
+    sum += (i + 1) * bruteForce(x, [1, 0, 0, 0], [0, 0, 0, 0], 30)
+
+print(sum)

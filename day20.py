65ef8879b23236e7ed20959eb018db77ba05f905
Simen Eilevstjønn <simene@lyse.net>
2022-12-20 10:06:53 +0100

    Change move method

--- day20.py
+++ day20.py
@@ -2,16 +2,33 @@ class EncryptedCoordinateNumber:
     def __init__(self, number):
         self.number = int(number)
 
-data = list(map(EncryptedCoordinateNumber, open("data20.txt", "r").read().split("\n")))
+data = list(map(EncryptedCoordinateNumber, open("testdata20.txt", "r").read().split("\n")))
 
 cpy = data[::]
 
 l = len(data)
 
-for n in cpy:
-    i = data.index(n)
+print(*map(lambda x : x.number, data))
+print()
 
-    data[i], data[(i + n.number) % l] = data[(i + n.number) % l], data[i]
+for n in cpy:
+    print("Active", n.number)
+    i = -1
+    for j in range(len(data)):
+        if data[j].number == n.number:
+            i = j
+            break
+
+    #data[i], data[(i + n.number) % l] = data[(i + n.number) % l], data[i]
+
+    data.pop(i)
+
+    data.insert((i + n.number) % l, n)
+
+
+    print(*map(lambda x : x.number, data))
+
+    print()
 
 
 idx = -1
@@ -21,5 +38,7 @@ for i in range(len(data)):
         break
 
 s = sum(data[(idx + i * 1000) % l].number for i in [1, 2, 3])
+
+print(*map(lambda x : x.number, data))
 
 print(s)

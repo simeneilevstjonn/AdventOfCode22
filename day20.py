8d4933d10d6c34d2f176de521fc8567ef73d2b40
Simen Eilevstjønn <simene@lyse.net>
2022-12-20 15:01:51 +0100

    Try custom implement

--- day20.py
+++ day20.py
@@ -2,17 +2,17 @@ class EncryptedCoordinateNumber:
     def __init__(self, number):
         self.number = int(number)
 
-data = list(map(EncryptedCoordinateNumber, open("testdata20.txt", "r").read().split("\n")))
+data = list(map(EncryptedCoordinateNumber, open("data20.txt", "r").read().split("\n")))
 
 cpy = data[::]
 
 l = len(data)
 
-print(*map(lambda x : x.number, data))
-print()
+#print(*map(lambda x : x.number, data))
+#print()
 
 for n in cpy:
-    print("Active", n.number)
+    #print("Active", n.number)
     i = -1
     for j in range(len(data)):
         if data[j].number == n.number:
@@ -21,14 +21,15 @@ for n in cpy:
 
     #data[i], data[(i + n.number) % l] = data[(i + n.number) % l], data[i]
 
-    data.pop(i)
+    sign = n.number // abs(n.number) if n.number != 0 else 0
 
-    data.insert((i + n.number) % l, n)
+    for j in range(abs(n.number)):
+        data[(i + sign * j) % l], data[(i + sign * (j + 1)) % l] = data[(i + sign * (j + 1)) % l], data[(i + sign * j) % l]
 
 
-    print(*map(lambda x : x.number, data))
+    #print(*map(lambda x : x.number, data))
 
-    print()
+    #print()
 
 
 idx = -1
@@ -39,6 +40,6 @@ for i in range(len(data)):
 
 s = sum(data[(idx + i * 1000) % l].number for i in [1, 2, 3])
 
-print(*map(lambda x : x.number, data))
+#print(*map(lambda x : x.number, data))
 
 print(s)

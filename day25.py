eb3c1b8d3725841585de04add1335569d15c5a91
Simen Eilevstjønn <simene@lyse.net>
2022-12-25 13:43:17 +0100

    Day 25 part 1

---
+++ day25.py
@@ -0,0 +1,36 @@
+def parseSNAFU(s):
+    n = 0
+    digits = {"0" : 0, "1": 1, "2": 2, "-" : -1, "=" : -2}
+
+    for i, d in enumerate(s):
+        n += digits[d] * 5 ** (len(s) - 1 - i)
+
+    return n
+
+number = sum(parseSNAFU(i) for i in open("data25.txt", "r").read().split("\n"))
+
+def decimalToSNAFU(n):
+    s = ""
+
+    while n != 0:
+        d = n % 5
+        carry = 0
+
+        if d < 3:
+            s = str(d + carry) + s
+        elif d == 3:
+            s = "=" + s
+            carry = 1
+        elif d == 4:
+            s = "-" + s
+            carry = 1
+
+        n //= 5
+        n += carry
+
+    return s
+
+print(decimalToSNAFU(number))
+
+
+
